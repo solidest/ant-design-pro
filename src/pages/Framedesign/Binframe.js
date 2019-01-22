c8e5b6e9c4f58318904f6cec8890895fc7e5dd68
solidest <solidest@163.com>
2019-01-22 01:41:40 -0800

    update

--- src/pages/Framedesign/Binframe.js
+++ src/pages/Framedesign/Binframe.js
@@ -9,30 +9,30 @@ import { Table } from 'antd';
 
 var code = 'OutInfoMsg:\n    Header:\n        SegmentType: uint16\n        ConstValue: 0xAA55\n    Len:\n';
     code += '        SegmentType: uint8\n        ConstValue: 22\n    Ctltoken:\n        SegmentType: uint8\n        ConstValue: 0xFB\n';
-    code += '    Position:\n        SegmentType: double\n    Weight:\n        SegmentType: double\n    Speed:\n        SegmentType: double\n';
+    code += '    Position:\n        SegmentType: float\n    Weight:\n        SegmentType: float\n    Speed:\n        SegmentType: float\n';
     code += '    SpeedTowards:\n    	SegmentType: uint8\n    SpeedWarnning:\n        SegmentType: uint8\n    WeightWarnning:\n        SegmentType: uint8\n';
-    code += '    BreakInfo:\n        SegmentType: uint8\n    Pullforce:\n    	SegmentType: double\n    OutTowards:\n    	SegmentType: uint8\n';
+    code += '    BreakInfo:\n        SegmentType: uint8\n    Pullforce:\n    	SegmentType: float\n    OutTowards:\n    	SegmentType: uint8\n';
     code += '    CheckSum:\n        SegmentType: uint8\n        Check: sum8\n    Ending:\n        SegmentType: uint16\n        ConstValue: 0xAA55\n';
-    code += '\nCmdMsg:\n     Header:\n        SegmentType: uint16\n        ConstValue: 0xAA55\n     Code:\n        SegmentType: uint8\n';
-    code += '        ConstValue: 0xFA\n     Unbreak:\n        SegmentType: uint8\n        ConstValue: 0x01\n     CheckSum:\n        SegmentType: uint8\n';
-    code += '        Check: sum8\n     Ending:\n        SegmentType: uint16\n        ConstValue: 0xAA55\n';
+var code2 = '\nCmdMsg:\n     Header:\n        SegmentType: uint16\n        ConstValue: 0xAA55\n     Code:\n        SegmentType: uint8\n';
+    code2 += '        ConstValue: 0xFA\n     Unbreak:\n        SegmentType: uint8\n        ConstValue: 0x01\n     CheckSum:\n        SegmentType: uint8\n';
+    code2 += '        Check: sum8\n     Ending:\n        SegmentType: uint16\n        ConstValue: 0xAA55\n';
 
 const columns = [
   {
-    title: '属性',
-    dataIndex: 'name',
-    key: 'name',
+    title: '字段',
+    dataIndex: 'segment',
+    key: 'segment',
   },
   {
-    title: '说明',
-    dataIndex: 'notes',
-    key: 'notes',
+    title: '类型',
+    dataIndex: 'type',
+    key: 'type',
     width: '25%',
   },
   {
-    title: '取值',
-    dataIndex: 'valuelist',
-    key: 'valuelist',
+    title: '属性',
+    dataIndex: 'pros',
+    key: 'pros',
     width: '50%',
   },
 ];
@@ -40,29 +40,78 @@ const columns = [
 const data = [
   {
     key: 1,
-    name: 'weight',
-    notes: '重量',
-    valuelist: '0.4, 0.5, 4.5, 4.6',
+    segment: 'Header',
+    type: 'uint16',
+    pros: '{ConstValue: 0xAA55}'
   },
   {
     key: 2,
-    name: 'posinfo',
-    notes: '位置信息',
-    children: [
-      {
-        key: 21,
-        name: 'start',
-        notes: '起始位置和方向',
-        valuelist:
-          '{p0: 0, towards: right}, {p0: 1, towards: right}, {p0: 1, towards: left}, {p0: 99, towards: right}, {p0: 99, towards: left}, {p0: 100, towards: left}',
-      },
-      {
-        key: 22,
-        name: 'speed',
-        notes: '行进速度',
-        valuelist: '0, 5, 5.1',
-      },
-    ],
+    segment: 'Len',
+    type: 'uint8',
+    pros: '{ConstValue: 22}'
+  },
+  {
+    key: 3,
+    segment: 'Ctltoken',
+    type: 'uint8',
+    pros: '{ConstValue: 0xFB}'
+  },
+  {
+    key: 4,
+    segment: 'Position',
+    type: 'float'
+  },
+  {
+    key: 5,
+    segment: 'Weight',
+    type: 'float'
+  },
+  {
+    key: 6,
+    segment: 'Speed',
+    type: 'float'
+  },
+  {
+    key: 7,
+    segment: 'Speedtowards',
+    type: 'uint8',
+  },
+  {
+    key: 8,
+    segment: 'SpeedWarnning',
+    type: 'uint8'
+  },
+  {
+    key: 9,
+    segment: 'WeightWarnning',
+    type: 'uint8'
+  },
+  {
+    key: 10,
+    segment: 'BreakInfo',
+    type: 'uint8'
+  },
+  {
+    key: 11,
+    segment: 'Pullforce',
+    type: 'float'
+  },
+  {
+    key: 12,
+    segment: 'OutTowards',
+    type: 'uint8'
+  },
+  {
+    key: 13,
+    segment: 'CheckSum',
+    type: 'uint8',
+    pros: '{Check: sum8}'
+  },
+  {
+    key: 14,
+    segment: 'Ending',
+    type: 'uint16',
+    pros: '{ConstValue: 0xAA55}'
   },
 ];
 
@@ -70,12 +119,12 @@ class Binframe extends Component {
   componentDidMount() {
     const reditor = this.refs.code_editor.editor;
     console.log(reditor);
-    reditor.setSize('100%', '580px');
+    reditor.setSize('100%', '800px');
   }
 
   render() {
     return (
-      <div style={{ display: 'flex', height: '580px' }}>
+      <div style={{ display: 'flex', height: '800px' }}>
         <div style={{ width: '40%', height: '100%', border: '1px solid #c7c7c7' }}>
           <CodeMirror
             ref="code_editor"
